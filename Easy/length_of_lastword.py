643e4e74a4bb993eed1d02c6b5bb94d0c91c2377
Humoyun Akbaraliev <136078810+humoyunswe@users.noreply.github.com>
2025-06-03 07:57:28 +0500

    add folder for Easy tasks with one problem

---
+++ Easy/length_of_lastword.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+
+        lst = s.strip()
+        return lst
+            
+sol = Solution()
+res = sol.lengthOfLastWord('   fly me   to   the moon  ')
+print(res)
